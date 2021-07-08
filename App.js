c7917a1e488ba054ada2f93f3b4b5e825596f030
hang long <sapbatcheung@gmail.com>
2021-07-08 16:12:37 +0700

    color

--- App.js
+++ App.js
@@ -16,97 +16,62 @@ import {
   Text,
   useColorScheme,
   View,
+  Dimensions,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const {width, height} = Dimensions.get('window');
+
+export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <React.Fragment>
+      <SafeAreaView style={styles.headerContainer}>
+        <View style={styles.header}>
+          <StatusBar backgroundColor="#61dafb" />
+          <Text style={styles.text}>Your Plan Today</Text>
+          <Icon name="delete" color="red" size={25} />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </SafeAreaView>
+      <SafeAreaView style={styles.bodyContainer}>
+        <View style={styles.body}></View>
+      </SafeAreaView>
+      <SafeAreaView style={styles.footer}>
+        <View style={styles.inputContainer}></View>
+      </SafeAreaView>
+    </React.Fragment>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  // Header Container
+  headerContainer: {
+    flex: 1,
+    backgroundColor: '#393E46',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  // Header
+  header: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    padding: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  // text
+  text: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#ee82ee',
   },
-  highlight: {
-    fontWeight: '700',
+  // footer container
+   
+  // footer
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    color: '#f5deb3',
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    paddingHorizontal: 20,
   },
 });
-
-export default App;
