ff8873f47410780c829804f3e84dc31c0146e885
hang long <sapbatcheung@gmail.com>
2021-07-08 16:49:13 +0700

    list

--- App.js
+++ App.js
@@ -6,8 +6,8 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {Component, useState} from 'react';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -17,6 +17,9 @@ import {
   useColorScheme,
   View,
   Dimensions,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -24,6 +27,21 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 const {width, height} = Dimensions.get('window');
 
 export default function App() {
+  const [todo, setTodos] = React.useState([
+    {id: 1, task: 'clean the house', completed: true},
+    {id: 2, task: 'kick some asses', completed: true},
+  ]);
+
+  const ListItem = ({todo}) => {
+    return (
+    <View style={styles.listItem}>
+      <View>
+        <Text>{todo?.task}</Text>
+      </View>
+    </View>
+    
+    );
+  };
   return (
     <React.Fragment>
       <SafeAreaView style={styles.headerContainer}>
@@ -32,12 +50,26 @@ export default function App() {
           <Text style={styles.text}>Your Plan Today</Text>
           <Icon name="delete" color="red" size={25} />
         </View>
-      </SafeAreaView>
-      <SafeAreaView style={styles.bodyContainer}>
+      
+      <View style={styles.bodyContainer}>
         <View style={styles.body}></View>
+      </View>
+      <FlatList
+        showVerticalScrollIndicator={false}
+        contentContainerStyle={{padding: 20, paddingBottom: 100}}
+        data={todo}
+        renderItem={({item}) => <ListItem todo={item} />}
+      />
       </SafeAreaView>
       <SafeAreaView style={styles.footer}>
-        <View style={styles.inputContainer}></View>
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Add your plan"></TextInput>
+        </View>
+        <TouchableOpacity>
+          <View style={styles.iconContainer}>
+            <Icon name="add" color="#ff6347" size={30} />
+          </View>
+        </TouchableOpacity>
       </SafeAreaView>
     </React.Fragment>
   );
@@ -62,8 +94,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#ee82ee',
   },
-  // footer container
-   
+
   // footer
   footer: {
     position: 'absolute',
@@ -73,5 +104,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: '100%',
     paddingHorizontal: 20,
+  },
+  // input container
+  inputContainer: {
+    backgroundColor: '#ff6347',
+    elevation: 40,
+    flex: 1,
+    height: 50,
+    marginVertical: 20,
+    marginRight: 20,
+    borderRadius: 30,
+    paddingHorizontal: 20,
+  },
+  // icon container
+  iconContainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: '#008080',
+    borderRadius: 25,
+    elevation: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  // List Item
+  listItem: {
+    padding: 20,
+    backgroundColor: '#008080',
+    flexDirection: 'row',
+    elevation: 12,
+    borderRadius: 7,
+    marginVertical: 10,
   },
 });
